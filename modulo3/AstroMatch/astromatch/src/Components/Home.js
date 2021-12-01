742158fcda4ec4500e0d23b113b84a9043c98f0a
Luis Gustavo <luisgustavordp1@gmail.com>
2021-12-01 19:23:09 -0300

    Segundo Commit

--- modulo3/AstroMatch/astromatch/src/Components/Home.js
+++ modulo3/AstroMatch/astromatch/src/Components/Home.js
@@ -1,5 +1,20 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
+import styled from 'styled-components';
+
+const Card = styled.div`
+  display: flex;
+  flex-direction: column;
+  box-shadow: 1px 3px 5px black;
+  align-items: center;
+  margin: 0 62vh;
+
+  img {
+    align-items: center;
+    width: 35vw;
+    height: 70vh;
+  }
+`;
 
 function Home(props) {
   const baseUrl =
@@ -8,14 +23,14 @@ function Home(props) {
   const autorizacao = 'luis-paula-carver';
 
   const [parametro, setParametro] = useState([]);
-  const [segundoParametro, setSegundoParametro] = useState({});
+  const [idUsuario, setIdUsuario] = useState('');
 
   useEffect(() => {
     chooseProfile();
-  }, []);
+  }, [setParametro]);
   useEffect(() => {
     choosePerson();
-  }, []);
+  }, [idUsuario]);
   useEffect(() => {
     clearEverything();
   }, []);
@@ -24,15 +39,16 @@ function Home(props) {
     axios
       .get(`${baseUrl}/${aluno}/person`)
       .then((res) => {
-        console.log(res.data.profile.name);
+        console.log(res.data.profile);
         setParametro(res.data.profile);
+        setIdUsuario(res.data.profile.id);
       })
       .catch((err) => {
         console.log(err);
       });
   };
   const choosePerson = () => {
-    const body = '';
+    const body = { id: idUsuario, choice: true };
     axios
       .post(`${baseUrl}/${aluno}/choose-person`, body, {
         headers: { Authorization: `${autorizacao}` },
@@ -57,11 +73,14 @@ function Home(props) {
   return (
     <div>
       <h1>Eu sou o Home</h1>
-      <div>
-        {parametro.map((perfil) => {
-          return <div key={perfil.id}>{perfil.name}</div>;
-        })}
-      </div>
+      <Card>
+        <h3>AstroMatch</h3>
+        <img src={parametro.photo} alt="foto" />
+        <p>
+          {parametro.name}, {parametro.age}
+        </p>
+        <p>{parametro.bio}</p>
+      </Card>
       <button onClick={props.irParaMatch}>Match</button>
     </div>
   );
