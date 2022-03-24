e00f2c5b0c2130d3508938b48436325da34caa5a
Luis Gustavo <luisgustavordp1@gmail.com>
2022-03-24 13:16:43 -0300

    Atualização Port

--- modulo6/case-1/Backend/src/index.ts
+++ modulo6/case-1/Backend/src/index.ts
@@ -8,8 +8,9 @@ const app = express();
 app.use(express.json());
 
 app.use('/api', pizzaRouter);
+const { PORT = 3003 } = process.env;
 
-const server = app.listen(3000, () => {
+const server = app.listen(PORT, () => {
   if (server) {
     const address = server.address() as AddressInfo;
     console.log(`Servidor rodando em http://localhost:${address.port}`);
