827a16e75bc815fcd7f8450c73f18efc8833cacf
Luis Gustavo <luisgustavordp1@gmail.com>
2022-03-25 18:10:52 -0300

    Requisição da api inicializada

--- modulo6/case-1/Frontend/case-1/src/pages/PizzasPage/PizzasPage.js
+++ modulo6/case-1/Frontend/case-1/src/pages/PizzasPage/PizzasPage.js
@@ -1,13 +1,25 @@
 import React from 'react';
-// import { useNavigate } from 'react-router-dom';
-import { getAllPizzas } from '../../services/getPizzas';
+import { useAllPizzas } from '../../services/getPizzas';
 
 export const PizzasPage = () => {
-  // const navigate = useNavigate();
-  console.log(getAllPizzas());
+  const pizza = useAllPizzas([]);
+
+  const pizzaCards = Object.keys(pizza).map((key) => {
+    return pizza[key].map((index) => {
+      return (
+        <div>
+          <p>{index.name}</p>
+          <p>{index.price}</p>
+          <p>{index.ingredients}</p>
+        </div>
+      );
+    });
+  });
+
   return (
     <div>
       <h1>Pizzas</h1>
+      <div>{pizzaCards}</div>
     </div>
   );
 };
--- modulo6/case-1/Frontend/case-1/src/services/getPizzas.js
+++ modulo6/case-1/Frontend/case-1/src/services/getPizzas.js
@@ -1,14 +1,20 @@
 import { BASE_URL } from '../constants/BASE_URL';
 import axios from 'axios';
+import { useEffect, useState } from 'react';
 
-export const getAllPizzas = () => {
-  axios
-    .get(`${BASE_URL}/api/pizzas`)
-    .then((res) => {
-      console.log(res.data);
-    })
-    .catch((err) => {
-      alert('Encontramos um erro, tente novamente!');
-      console.log(err);
-    });
+export const useAllPizzas = (initialData) => {
+  const [data, setData] = useState(initialData);
+  useEffect(() => {
+    axios
+      .get(`${BASE_URL}/api/pizzas`)
+      .then((res) => {
+        console.log(res.data);
+        setData(res.data);
+      })
+      .catch((err) => {
+        alert('Encontramos um erro, tente novamente!');
+        console.log(err);
+      });
+  }, []);
+  return data;
 };
