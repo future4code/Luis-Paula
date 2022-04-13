c06fe17efbacb3bea3904a56641393f849852d66
Luis Gustavo <luisgustavordp1@gmail.com>
2022-04-13 17:03:18 -0300

    Tipos e DataBase feitos

--- modulo6/case-4/src/Data/BaseDataBase.ts
+++ modulo6/case-4/src/Data/BaseDataBase.ts
@@ -4,16 +4,22 @@ import knex from 'knex';
 
 dotenv.config();
 
-export class Connection {
-  protected static connection: Knex = knex({
-    client: 'mysql',
-    connection: {
-      host: process.env.DB_HOST,
-      port: 3306,
-      user: process.env.DB_USER,
-      password: process.env.DB_PASSWORD,
-      database: process.env.DB_SCHEMA,
-      multipleStatements: true,
-    },
-  });
+export class BaseDataBase {
+  protected static connection: Knex | null = null;
+  protected getConnection(): Knex {
+    if (!BaseDataBase.connection) {
+      BaseDataBase.connection = knex({
+        client: 'mysql',
+        connection: {
+          host: process.env.DB_HOST,
+          port: 3306,
+          user: process.env.DB_USER,
+          password: process.env.DB_PASSWORD,
+          database: process.env.DB_SCHEMA,
+          multipleStatements: true,
+        },
+      });
+    }
+    return BaseDataBase.connection;
+  }
 }
---
+++ modulo6/case-4/src/Model/TypesJavelin.ts
@@ -0,0 +1,14 @@
+export type Javelin = {
+  id: string;
+  competicao: string;
+  atleta: string;
+  value: number;
+  unidade: string;
+};
+
+export type JavelinDTO = {
+  competicao: string;
+  atleta: string;
+  value: number;
+  unidade: string;
+};
